9d72a3adcb7e22cd322fa8f1110aa326abcba046
Emerson Demetrio <emerson@drivekyte.com>
2024-08-01 15:58:43 +0200

    [0000] Organize folder

--- helpers/types.ts
+++
@@ -1,3 +0,0 @@
-export type GenericObject = {
-    [key: string]: number | string | boolean | null | undefined | GenericObject | GenericObject[];
-}
---
+++ src/helpers/types.ts
@@ -0,0 +1,10 @@
+export type GenericObject = {
+  [key: string]:
+  | number
+  | string
+  | boolean
+  | null
+  | undefined
+  | GenericObject
+  | GenericObject[];
+}
